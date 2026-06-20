3aed5f0530b46222896f55ac22f34d53a2fd6575
Dilara Sultanova <dilara.sultanova75@gmail.com>
2026-06-20 15:41:30 +0300

    Update scraper.py

--- github-job-scraper/scraper.py
+++ github-job-scraper/scraper.py
@@ -1,16 +1,26 @@
 import requests
 import json
 
-url = "https://jobs.github.com/positions.json?description=python"
+URL = "https://remotive.com/api/remote-jobs?search=python"
 
 try:
-    response = requests.get(url)
-    jobs = response.json()
+    response = requests.get(URL)
+    data = response.json()
+
+    jobs = []
+
+    for job in data["jobs"][:10]:
+        jobs.append({
+            "title": job["title"],
+            "company": job["company_name"],
+            "location": job["candidate_required_location"],
+            "url": job["url"]
+        })
 
     with open("jobs.json", "w") as file:
         json.dump(jobs, file, indent=4)
 
-    print(f"Saved {len(jobs)} jobs to jobs.json")
+    print(f"Saved {len(jobs)} jobs.")
 
 except Exception as e:
     print("Error:", e)
